5b46d8e93cdd54487fea4ceb9f4158829bc43c83
Tb2019 <982148620@qq.com>
2025-02-13 17:19:42 +0800

    新思路首次提交

--- exhibition.py
+++ exhibition.py
@@ -5,13 +5,51 @@ import requests
 from lxml import etree
 from functools import lru_cache
 
-def get_category_info():
-	index_url = 'https://www.chem17.com/exhibition/t0/list_ch0_pid0.html'
+
+def get_dict(page_tree, category_spec_name):
+	info_dict = {}
+	items = page_tree.xpath('//div[@class="main"]//div[@class="left"]/ul/li')
+	for item in items:
+		name = item.xpath('./div[@class="text"]//a/text()')[0]
+		info_dict.setdefault(name, []).append(category_spec_name)
+	return info_dict
+
+
+def get_info(category_name):
+	info_dict = {}
+	index_url = 'https://www.chem17.com/exhibition/t0/list.html'
 	index = get_index(index_url)
 
 	page_tree = etree.HTML(index)
-	category_eles = page_tree.xpath('//ul[@class="public-category showone"]')[0].xpath('./li[position()>1]/a')  # 分类ul标签
-	province_eles = page_tree.xpath('//ul[@class="public-category showone"]')[1].xpath('./li[position()>1]/a')  # 省市ul标签
+	if category_name == 'field':
+		eles = page_tree.xpath('//ul[@class="public-category showone"]')[0].xpath('./li[position()>1]/a')  # 分类ul标签
+	else:
+		eles = page_tree.xpath('//ul[@class="public-category showone"]')[1].xpath('./li[position()>1]/a')  # 省市ul标签
+
+	for ele in eles:
+		name = ele.xpath('./text()')[0]
+		url = ele.xpath('./@href')[0]
+
+		index = get_index(url)  # 分类首页
+		index_tree = etree.HTML(index)
+
+		try:
+			page_info = index_tree.xpath('//div[@class="pages"]/span[@class="jump"]/text()')[0]
+		except:
+			continue
+		else:
+			info_dict.update(get_dict(index_tree, name))
+
+			totle_pages = int(re.search(r'共(\d)页', page_info).group(1))
+			for page in range(2, totle_pages + 1):
+				url_head = url.rsplit('/', 1)[0]
+				next_url = url_head + f'_p{page}'
+				next_page = requests.get(next_url)
+				next_page_tree = etree.HTML(next_page.text)
+				next_info_dict = get_dict(next_page_tree, name)
+				info_dict.update(next_info_dict)
+		# break
+	return info_dict
 
 
 # @lru_cache
@@ -29,7 +67,7 @@ def parse_detail(detail_url):
 	content = etree.tostring(content_ele, encoding='utf-8').decode('utf-8')
 	return content
 
-def parse_category(page_tree: etree.ElementTree):
+def parse_index(page_tree: etree.ElementTree):
 	items = page_tree.xpath('//div[@class="main"]//div[@class="left"]/ul/li')
 	for item in items:
 		field = '展览'
@@ -40,7 +78,7 @@ def parse_category(page_tree: etree.ElementTree):
 		time = item.xpath('./div[@class="text"]/div[@class="bottom"]/b[1]/text()')[0]
 		year = re.match(r'^\d{4}', time).group()
 		time = re.sub(r'-', '/', time)
-		time = re.sub(r'~', '--', time)
+		time = re.sub(r' ~ ', '--', time)
 
 		place = item.xpath('./div[@class="text"]/div[@class="bottom"]/b[2]/text()')[0]
 		type = '线下'
@@ -56,33 +94,18 @@ def parse_category(page_tree: etree.ElementTree):
 		break
 
 def main():
-	index_url = 'https://www.chem17.com/exhibition/t0/list_ch0_pid0.html'
-	index = get_index(index_url)
+	index_urls = ['https://www.chem17.com/exhibition/t0/list_ch0_pid0.html',
+				 'https://www.chem17.com/exhibition/t0/list_p2.html',
+				 'https://www.chem17.com/exhibition/t0/list_p3.html']
+	for index_url in index_urls:
+		index = get_index(index_url)
 
-	page_tree = etree.HTML(index)
-	category_eles = page_tree.xpath('//ul[@class="public-category showone"]')[0].xpath('./li[position()>1]/a')  # 分类ul标签
-	province_eles = page_tree.xpath('//ul[@class="public-category showone"]')[1].xpath('./li[position()>1]/a')  # 省市ul标签
-
-	for category_ele in category_eles:
-		category_name = category_ele.xpath('./text()')[0]
-		category_url = category_ele.xpath('./@href')[0]
-
-		category_index = get_index(category_url)  # 分类首页
-		category_index_tree = etree.HTML(category_index)
-
-		try:
-			page_info = category_index_tree.xpath('//div[@class="pages"]/span[@class="jump"]/text()')[0]
-		except:
-			continue
-		else:
-			totle_pages = int(re.search(r'共(\d)页', page_info).group(1))
-			if totle_pages == 1:
-				parse_category(category_index_tree)
-			else:
-				pass
-			break
+		page_tree = etree.HTML(index)
+		parse_index(page_tree)
 
 
 
 if __name__ == '__main__':
-	main()
+	# main()
+	res = get_info('field')
+	print(res)
